4d1d8825bc368482b414c514cc2e11265b89a9a5
GuillianV <guillianvibert90@gmail.com>
2022-12-29 11:20:14 +0100

    add data on connexion

--- hc05.ino
+++ hc05.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial mySerial(10, 11);  // RX, TX
+SoftwareSerial mySerial(10, 11);  // TX, RX
 const byte PIN_LED_R = 6;
 const byte PIN_LED_G = 5;
 const byte PIN_LED_B = 3;
@@ -10,10 +10,12 @@ byte rgbAlerte[3] = { 255, 30, 0 };
 byte rgbDanger[3] = { 255, 0, 0 };
 
 byte alertState = 0;
-
+String alertStatus = "V_calme";
 String content = "";
 char character;
 
+boolean isConnected = false;
+boolean sendEtatAlerte = false;
 void setup() {
   Serial.begin(57600);
   pinMode(9, OUTPUT);
@@ -30,10 +32,17 @@ void loop() {
 
   if (mySerial.available()) {
 
+
+    if (sendEtatAlerte == false) {
+      sendEtatAlerte = true;
+      writeString("ptc_alerte_" +alertStatus);
+    }
     while (mySerial.available()) {
       character = mySerial.read();
       content.concat(character);
     }
+    Serial.println(content);
+
     updateAlerteState(content);
   }
 
@@ -43,18 +52,28 @@ void loop() {
   }
 }
 
+
+
+void writeString(String stringData) {  // Used to serially push out a String with Serial.write()
+
+  for (int i = 0; i < stringData.length(); i++) {
+    mySerial.write(stringData[i]);  // Push each char 1 by 1 on each loop pass
+  }
+}
+
+
 void updateAlerteState(String alerteMessage) {
 
   if (alerteMessage.indexOf("ptc_alerte_") == -1) {
     return;
   }
 
-    
-  alerteMessage.remove(0, alerteMessage.indexOf("ptc_alerte_")+11);
-  Serial.println(alerteMessage);
+
+  alerteMessage.remove(0, alerteMessage.indexOf("ptc_alerte_") + 11);
 
   if (alerteMessage == "V_calme") {
     Serial.println(alerteMessage);
+    writeString("ptc_alerte_V_calme");
     content = "";
     alertState = 0;
     displayColor(rgbCalme);
@@ -62,13 +81,15 @@ void updateAlerteState(String alerteMessage) {
 
   if (alerteMessage == "V_alerte") {
     Serial.println(alerteMessage);
+    writeString("ptc_alerte_V_alerte");
     content = "";
     alertState = 1;
     displayColor(rgbAlerte);
   }
 
   if (alerteMessage == "V_danger") {
- 
+    Serial.println(alerteMessage);
+    writeString("ptc_alerte_V_danger");
     content = "";
     alertState = 2;
     displayColor(rgbDanger);
